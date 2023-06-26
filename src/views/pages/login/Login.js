20ead4ac8d41e5cca06d2759e1d42b90f56aa96e
Rahul <rahulkirti10@gmail.com>
2023-06-26 22:45:23 +0530

    project setup

---
+++ src/views/pages/login/Login.js
@@ -0,0 +1,149 @@
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import * as Loader from 'react-loader-spinner';
+import {
+  CButton,
+  CCard,
+  CCardBody,
+  CCardGroup,
+  CCol,
+  CContainer,
+  CForm,
+  CFormInput,
+  CInputGroup,
+  CInputGroupText,
+  CRow,
+} from '@coreui/react';
+import CIcon from '@coreui/icons-react';
+import { cilLockLocked, cilUser } from '@coreui/icons';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+const Login = () => {
+  const navigate = useNavigate();
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [errorMessage, setErrorMessage] = React.useState('');
+  const [showComponent, setShowComponent] = React.useState(false); // New state variable
+  const [showloader, setShowLoader] = React.useState(true);
+  const apiUrl = process.env.REACT_APP_ADMIN_API_URL;
+
+  useEffect(() => {
+    axios.get(`${apiUrl}api/v1/admin/getProfile`, { withCredentials: true })
+      .then(response => {
+        navigate('/dashboard');
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log(error.response);
+        setShowLoader(false)
+        setShowComponent(true); // Set showComponent to true if the API call fails
+      });
+  }, []);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    if (username === '') {
+      setErrorMessage('Please enter a valid username!');
+    } else if (password === '') {
+      setErrorMessage('Please enter a valid password!');
+    } else {
+      const data = {
+        email: username,
+        password: password
+      };
+
+      // Make the POST request
+      axios.post(`${apiUrl}api/v1/admin/login`, data, { withCredentials: true })
+        .then(response => {
+          navigate('/dashboard');
+        })
+        .catch(error => {
+          console.log(error.response);
+          setErrorMessage(error.response.data.message);
+        });
+    }
+  };
+
+  return (
+    <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
+       {showloader ? (
+      <div style={{justifyContent:'center', marginLeft:'auto', marginRight:'auto', textAlign: 'center', fontSize:'20px'}}><Loader.TailSpin color="black" width='250px'/> 
+      <p>Loading your dashboard...</p>
+      </div>
+      ) : null}
+      {showComponent ? (
+      <CContainer>
+        <CRow className="justify-content-center">
+          <CCol md={8}>
+            <CCardGroup>
+              <CCard className="p-4">
+                <CCardBody>
+                  
+                    <CForm>
+                      <h1>Login</h1>
+                      <p className="text-medium-emphasis">Sign In to your account</p>
+                      <CInputGroup className="mb-3">
+                        <CInputGroupText>
+                          <CIcon icon={cilUser} />
+                        </CInputGroupText>
+                        <CFormInput
+                          placeholder="Username"
+                          autoComplete="username"
+                          value={username}
+                          onChange={(e) => setUsername(e.target.value)}
+                        />
+                      </CInputGroup>
+                      <CInputGroup className="mb-4">
+                        <CInputGroupText>
+                          <CIcon icon={cilLockLocked} />
+                        </CInputGroupText>
+                        <CFormInput
+                          type="password"
+                          placeholder="Password"
+                          autoComplete="current-password"
+                          value={password}
+                          onChange={(e) => setPassword(e.target.value)}
+                        />
+                      </CInputGroup>
+                      <CRow>
+                        <CCol>
+                          <div style={{ color: 'red', margin: '0px 0px 15px 0px' }}>
+                            {errorMessage && <div> {errorMessage} </div>}
+                          </div>
+                        </CCol>
+                      </CRow>
+                      <CRow>
+                        <CCol xs={6}>
+                          <CButton type="submit" color="primary" className="px-4" onClick={handleClick}>
+                            Login
+                          </CButton>
+                        </CCol>
+                        <CCol xs={6} className="text-right">
+                          <CButton color="link" className="px-0">
+                            Forgot password?
+                          </CButton>
+                        </CCol>
+                      </CRow>
+                    </CForm>
+                 
+                </CCardBody>
+              </CCard>
+              <CCard className="text-white bg-primary py-5" style={{ width: '44%' }}>
+                <CCardBody className="text-center">
+                  <div>
+                    <h2>Login</h2>
+                    <p>Admin login. Only for authenticated people. If you have admin credentials, please login.</p>
+                  </div>
+                </CCardBody>
+              </CCard>
+            </CCardGroup>
+          </CCol>
+        </CRow>
+      </CContainer>
+       ) : null}
+    </div>
+  );
+};
+
+export default Login;
